7b7ec0b587b9d95e66d1a91fb787b6b665c2c5df
RISHAV RAJ <RISHAVRAJ74883@GMAIL.COM>
2026-09-03 18:43:58 +0530

    Add root one-command Windows installer

---
+++ install.ps1
@@ -0,0 +1,13 @@
+$ErrorActionPreference = 'Stop'
+$Repo = 'https://github.com/Rishav7324/web-builder.git'
+$Tmp = Join-Path $env:TEMP ("web-builder-install-" + [guid]::NewGuid().ToString())
+try {
+  if (-not (Get-Command git -ErrorAction SilentlyContinue)) { throw 'git is required.' }
+  if (-not (Get-Command python -ErrorAction SilentlyContinue)) { throw 'Python 3 is required.' }
+  Write-Host 'Installing Web Builder...'
+  git clone --depth 1 --quiet $Repo $Tmp
+  & python (Join-Path $Tmp 'installer/install.py') --targets all @args
+  Write-Host 'Web Builder installed successfully.'
+} finally {
+  if (Test-Path $Tmp) { Remove-Item -Recurse -Force $Tmp }
+}
